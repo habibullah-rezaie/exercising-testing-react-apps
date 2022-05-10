7610d2f1583d96f0fd90da31287b472eefd970ab
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-10 16:02:53 +0430

    Ensure onSubmit is called with password & username

--- src/__tests__/exercise/04.tsx
+++ src/__tests__/exercise/04.tsx
@@ -1,27 +1,38 @@
 // form testing
 // http://localhost:3000/login
 
-import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import * as React from 'react'
 import Login from '../../components/login'
 
-test('submitting the form calls onSubmit with username and password', () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
-  // 🐨 use `await userEvent.type...` to change the username and password fields to
-  //    whatever you want
-  //
-  // 🐨 click on the button with the text "Submit"
-  //
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+test('submitting the form calls onSubmit with username and password', async () => {
+  type loginData = {
+    password: string
+    username: string
+  }
+
+  let submittedData: null | loginData = null
+
+  function handleSubmit(data: loginData) {
+    submittedData = data
+  }
+
+  render(<Login onSubmit={handleSubmit} />)
+
+  screen.debug()
+  const usernameField = screen.getByLabelText('Username')
+  const passwordField = screen.getByLabelText('Password')
+  const submitBtn = screen.getByRole('button')
+
+  await userEvent.type(usernameField, 'my-username')
+  await userEvent.type(passwordField, 'my-password')
+
+  await userEvent.click(submitBtn)
+  expect(submittedData).toEqual({
+    username: 'my-username',
+    password: 'my-password',
+  })
 })
 
 /*
