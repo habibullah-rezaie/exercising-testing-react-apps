40244414690624479144074a7be84ace8883a23a
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-10 21:28:35 +0430

    use Test Data Bot

--- src/__tests__/exercise/04.tsx
+++ src/__tests__/exercise/04.tsx
@@ -2,14 +2,23 @@
 // http://localhost:3000/login
 
 import {render, screen} from '@testing-library/react'
-import * as faker from 'faker'
 import userEvent from '@testing-library/user-event'
 import * as React from 'react'
 import Login from '../../components/login'
+import {build, fake} from '@jackfranklin/test-data-bot'
+
+const buildTestLoginData = build({
+  fields: {
+    username: fake(faker => faker.internet.userName()),
+    password: fake(faker => faker.internet.password()),
+  },
+})
 
 test('submitting the form calls onSubmit with username and password', async () => {
   const {username, password} = buildTestLoginData({
-    password: 'Akjfslkfew909809rw.kljf',
+    overrides: {
+      username: 'hikjlk'
+    }
   }) // Overwrite the password to have a strong password
 
   const handleSubmit = jest.fn()
@@ -24,15 +33,7 @@ test('submitting the form calls onSubmit with username and password', async () =
   await userEvent.type(usernameField, username)
   await userEvent.click(submitBtn)
 
+  console.log(username, password)
   expect(handleSubmit).toHaveBeenCalledWith({username, password})
   expect(handleSubmit).toHaveBeenCalledTimes(1)
 })
-
-function buildTestLoginData(loginData?: {username?: string; password?: string}) {
-  const loginTestData = {
-    username: loginData?.username || faker.internet.userName(),
-    password: loginData?.password || faker.internet.password(),
-  }
-
-  return {...loginTestData}
-}
