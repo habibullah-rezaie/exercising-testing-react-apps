eb1655ab899f0f80bf380a6ddcc39f077008d63c
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-10 17:53:24 +0430

    Generate random username & password

--- src/__tests__/exercise/04.tsx
+++ src/__tests__/exercise/04.tsx
@@ -2,23 +2,21 @@
 // http://localhost:3000/login
 
 import {render, screen} from '@testing-library/react'
+import * as faker from 'faker'
 import userEvent from '@testing-library/user-event'
 import * as React from 'react'
 import Login from '../../components/login'
 
 test('submitting the form calls onSubmit with username and password', async () => {
-  const handleSubmit = jest.fn()
-  const loginTestData = {
-    username: 'my-username',
-    password: 'my-password',
-  }
+  const {
+    handleSubmit,
+    passwordField,
+    usernameField,
+    loginTestData,
+    submitBtn,
+  } = buildLoginForm()
 
-  render(<Login onSubmit={handleSubmit} />)
-
-  const usernameField = screen.getByRole('textbox', {name: /username/i})
-  const passwordField = screen.getByLabelText('Password')
-  const submitBtn = screen.getByRole('button', {name: /submit/i})
-
+  console.dir(loginTestData)
   await userEvent.type(passwordField, loginTestData.password)
   await userEvent.type(usernameField, loginTestData.username)
 
@@ -26,7 +24,18 @@ test('submitting the form calls onSubmit with username and password', async () =
   expect(handleSubmit).toHaveBeenCalledWith(loginTestData)
 })
 
-/*
-eslint
-  no-unused-vars: "off",
-*/
+function buildLoginForm() {
+  const loginTestData = {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+  }
+
+  const handleSubmit = jest.fn()
+  render(<Login onSubmit={handleSubmit} />)
+
+  const usernameField = screen.getByRole('textbox', {name: /username/i})
+  const passwordField = screen.getByLabelText('Password')
+  const submitBtn = screen.getByRole('button', {name: /submit/i})
+
+  return {handleSubmit, loginTestData, usernameField, passwordField, submitBtn}
+}
