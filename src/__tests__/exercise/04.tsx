8a6e3ff5c8ee79e9c2383163174f1d9748160515
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-10 18:46:22 +0430

    Limit 'buildLoginForm' to only generate test login data

--- src/__tests__/exercise/04.tsx
+++ src/__tests__/exercise/04.tsx
@@ -8,30 +8,31 @@ import * as React from 'react'
 import Login from '../../components/login'
 
 test('submitting the form calls onSubmit with username and password', async () => {
-  const {handleSubmit, passwordField, usernameField, loginTestData, submitBtn} =
-    buildLoginForm({password: 'Akjfslkfew909809rw.kljf'}) // Overwrite the password to have a strong password
-
-  console.dir(loginTestData)
-  await userEvent.type(passwordField, loginTestData.password)
-  await userEvent.type(usernameField, loginTestData.username)
-
-  await userEvent.click(submitBtn)
-  expect(handleSubmit).toHaveBeenCalledWith(loginTestData)
-  expect(handleSubmit).toHaveBeenCalledTimes(1)
-})
-
-function buildLoginForm(loginData?: {username?: string; password?: string}) {
-  const loginTestData = {
-    username: loginData?.username || faker.internet.userName(),
-    password: loginData?.password || faker.internet.password(),
-  }
+  const {username, password} = buildTestLoginData({
+    password: 'Akjfslkfew909809rw.kljf',
+  }) // Overwrite the password to have a strong password
 
   const handleSubmit = jest.fn()
+
   render(<Login onSubmit={handleSubmit} />)
 
   const usernameField = screen.getByRole('textbox', {name: /username/i})
   const passwordField = screen.getByLabelText('Password')
   const submitBtn = screen.getByRole('button', {name: /submit/i})
 
-  return {handleSubmit, loginTestData, usernameField, passwordField, submitBtn}
+  await userEvent.type(passwordField, password)
+  await userEvent.type(usernameField, username)
+  await userEvent.click(submitBtn)
+
+  expect(handleSubmit).toHaveBeenCalledWith({username, password})
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+})
+
+function buildTestLoginData(loginData?: {username?: string; password?: string}) {
+  const loginTestData = {
+    username: loginData?.username || faker.internet.userName(),
+    password: loginData?.password || faker.internet.password(),
+  }
+
+  return {...loginTestData}
 }
