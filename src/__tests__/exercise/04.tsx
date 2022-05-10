cbfddfd4aad4908ca324184effe8780abbc2c154
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-10 16:51:00 +0430

    Bring some afterthought changes

--- src/__tests__/exercise/04.tsx
+++ src/__tests__/exercise/04.tsx
@@ -1,38 +1,29 @@
 // form testing
 // http://localhost:3000/login
 
-import { render, screen } from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import * as React from 'react'
 import Login from '../../components/login'
 
 test('submitting the form calls onSubmit with username and password', async () => {
-  type loginData = {
-    password: string
-    username: string
-  }
-
-  let submittedData: null | loginData = null
-
-  function handleSubmit(data: loginData) {
-    submittedData = data
+  const handleSubmit = jest.fn()
+  const loginTestData = {
+    username: 'my-username',
+    password: 'my-password',
   }
 
   render(<Login onSubmit={handleSubmit} />)
 
-  screen.debug()
-  const usernameField = screen.getByLabelText('Username')
+  const usernameField = screen.getByRole('textbox', {name: /username/i})
   const passwordField = screen.getByLabelText('Password')
-  const submitBtn = screen.getByRole('button')
+  const submitBtn = screen.getByRole('button', {name: /submit/i})
 
-  await userEvent.type(usernameField, 'my-username')
-  await userEvent.type(passwordField, 'my-password')
+  await userEvent.type(passwordField, loginTestData.password)
+  await userEvent.type(usernameField, loginTestData.username)
 
   await userEvent.click(submitBtn)
-  expect(submittedData).toEqual({
-    username: 'my-username',
-    password: 'my-password',
-  })
+  expect(handleSubmit).toHaveBeenCalledWith(loginTestData)
 })
 
 /*
