3e2dd53232392a5283a0f735404441f74481696e
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-10 14:23:33 +0430

    Use userEvent to simulate user interaction

--- src/__tests__/exercise/03.tsx
+++ src/__tests__/exercise/03.tsx
@@ -1,8 +1,9 @@
 // Avoid implementation details
 // http://localhost:3000/counter
 
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import * as React from 'react'
-import {fireEvent, render, screen} from '@testing-library/react'
 import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', async () => {
@@ -12,9 +13,9 @@ test('counter increments and decrements when the buttons are clicked', async () 
   const increment = screen.getByRole('button', {name: /increment/i})
   const message = screen.getByText('Current count: 0')
 
-  fireEvent.click(increment)
+  await userEvent.click(increment)
   expect(message).toHaveTextContent('Current count: 1')
 
-  fireEvent.click(decrement)
+  await userEvent.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
 })
