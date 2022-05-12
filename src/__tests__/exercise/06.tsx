553fec23df41d087ca9d507dc5a452715c71a12b
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-12 12:25:48 +0430

    Test 'Location' displays usrs current location

--- src/__tests__/exercise/06.tsx
+++ src/__tests__/exercise/06.tsx
@@ -1,71 +1,56 @@
 // mocking Browser APIs and modules
 // http://localhost:3000/location
 
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import * as React from 'react'
-import {render, screen, act} from '@testing-library/react'
 import Location from '../../examples/location'
 
-// 🐨 set window.navigator.geolocation to an object that has a getCurrentPosition mock function
+const mockedGeolocation = {getCurrentPosition: jest.fn()}
 
-// 💰 I'm going to give you this handy utility function
-// it allows you to create a promise that you can resolve/reject on demand.
+beforeAll(() => {
+  Object.defineProperty(window.navigator, 'geolocation', {
+    value: mockedGeolocation,
+  })
+})
+
 function deferred() {
-  let resolve, reject
+  let resolve: undefined | ((value?: unknown) => void)
+  let reject: undefined | ((value?: unknown) => void)
+
   const promise = new Promise((res, rej) => {
     resolve = res
     reject = rej
   })
   return {promise, resolve, reject}
 }
-// 💰 Here's an example of how you use this:
-// const {promise, resolve, reject} = deferred()
-// promise.then(() => {/* do something */})
-// // do other setup stuff and assert on the pending state
-// resolve()
-// await promise
-// // assert on the resolved state
 
 test('displays the users current location', async () => {
-  // 🐨 create a fakePosition object that has an object called "coords" with latitude and longitude
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/GeolocationPosition
-  //
-  // 🐨 create a deferred promise here
-  //
-  // 🐨 Now we need to mock the geolocation's getCurrentPosition function
-  // To mock something you need to know its API and simulate that in your mock:
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
-  //
-  // here's an example of the API:
-  // function success(position) {}
-  // function error(error) {}
-  // navigator.geolocation.getCurrentPosition(success, error)
-  //
-  // 🐨 so call mockImplementation on getCurrentPosition
-  // 🐨 the first argument of your mock should accept a callback
-  // 🐨 you'll call the callback when the deferred promise resolves
-  // 💰 promise.then(() => {/* call the callback with the fake position */})
-  //
-  // 🐨 now that setup is done, render the Location component itself
-  //
-  // 🐨 verify the loading spinner is showing up
-  // 💰 tip: try running screen.debug() to know what the DOM looks like at this point.
-  //
-  // 🐨 resolve the deferred promise
-  // 🐨 wait for the promise to resolve
-  // 💰 right around here, you'll probably notice you get an error log in the
-  // test output. You can ignore that for now and just add this next line:
-  // act(() => {})
-  //
-  // If you'd like, learn about what this means and see if you can figure out
-  // how to make the warning go away (tip, you'll need to use async act)
-  // 📜 https://kentcdodds.com/blog/fix-the-not-wrapped-in-act-warning
-  //
-  // 🐨 verify the loading spinner is no longer in the document
-  //    (💰 use queryByLabelText instead of getByLabelText)
-  // 🐨 verify the latitude and longitude appear correctly
-})
+  const fakePosition = {coords: {latitude: 0, longitude: 3}}
 
-/*
-eslint
-  no-unused-vars: "off",
-*/
+  const {promise, resolve} = deferred()
+
+  mockedGeolocation.getCurrentPosition.mockImplementation(onSuccess => {
+    promise.then(() => {
+      onSuccess(fakePosition)
+    })
+  })
+
+  render(<Location />)
+  const loadingSpinner = screen.getByLabelText(/loading/i)
+  expect(loadingSpinner).toBeInTheDocument()
+
+  resolve && resolve()
+
+  await waitForElementToBeRemoved(loadingSpinner)
+
+  expect(loadingSpinner).not.toBeInTheDocument()
+
+  screen.debug()
+
+  expect(screen.getByText(/latitude/i)).toHaveTextContent(
+    `Latitude: ${fakePosition.coords.latitude}`,
+  )
+  expect(screen.getByText(/longitude/i)).toHaveTextContent(
+    `Longitude: ${fakePosition.coords.longitude}`,
+  )
+})
