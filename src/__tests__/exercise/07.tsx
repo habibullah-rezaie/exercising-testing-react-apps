0ce6fe2a5d7bf84820ce34686a077fd72e67b4af
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-05-13 09:11:04 +0430

    Make 'renderWithThemes' to accept original 'render' optoins

--- src/__tests__/exercise/07.tsx
+++ src/__tests__/exercise/07.tsx
@@ -2,16 +2,9 @@
 // http://localhost:3000/easy-button
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, RenderOptions, screen} from '@testing-library/react'
 import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
-
-function renderWithTheme(comp: JSX.Element, theme: string) {
-  function Wrapper({children}: React.PropsWithChildren<{}>) {
-    return <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
-  }
-  return render(comp, {wrapper: Wrapper})
-}
 
 type Themes = {
   dark: React.CSSProperties
@@ -27,7 +20,7 @@ const themeStyles: Themes = {
 test('renders with the light styles for the light theme', () => {
   const lightTheme = 'light'
 
-  renderWithTheme(<EasyButton>Easy</EasyButton>, lightTheme)
+  renderWithTheme(<EasyButton>Easy</EasyButton>)
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: ${themeStyles[lightTheme].backgroundColor};
@@ -37,13 +30,25 @@ test('renders with the light styles for the light theme', () => {
 
 test('renders with the dark styles for the dark theme', () => {
   const darkTheme = 'dark'
-  
 
-  renderWithTheme(<EasyButton>Easy</EasyButton>, darkTheme)
+  renderWithTheme(<EasyButton>Easy</EasyButton>, {theme: darkTheme})
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: ${themeStyles[darkTheme].backgroundColor};
     color: ${themeStyles[darkTheme].color};
   `)
 })
+
+function renderWithTheme(
+  comp: JSX.Element,
+  {
+    theme = 'light',
+    ...options
+  }: Omit<RenderOptions, 'wrapper'> & {theme?: string} = {},
+) {
+  function Wrapper({children}: React.PropsWithChildren<{}>) {
+    return <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
+  }
+  return render(comp, {wrapper: Wrapper, ...options})
+}
 /* eslint no-unused-vars:0 */
